5ac95ca2215e52b6406e4af22490bf7cf8aeea82
Pranav Krishnaa <krishnaapranav576@gmail.com>
2026-03-28 23:33:49 +0530

    final last

--- blog-project-starter-frontend/src/components/Blogs.jsx
+++ blog-project-starter-frontend/src/components/Blogs.jsx
@@ -1,3 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import Footer from './common/Footer';
+import auth from '../config/firebase';
+
 const API_URL = "https://blog-project-work-kj7d.vercel.app/api/blogs";
 
 const fetchBlogs = () => {
